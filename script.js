894dd9eee233278fd6052e6abeacaaa3143697ac
KASIM BHATE <devloperkasim@gmail.com>
2024-04-28 15:42:44 +0530

    added files for assessment two

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+
+function increment() {
+	event.preventDefault();
+    let quantityElement = document.getElementById('quantity'); 
+    let quantity = parseInt(quantityElement.textContent);
+    quantity++;
+    quantityElement.innerText = quantity;
+  }
+
+  // Function to decrement quantity
+  function decrement() {
+    var quantityElement = document.getElementById('quantity');
+    var quantity = parseInt(quantityElement.innerText);
+    if (quantity > 1) {
+      quantity--;
+      quantityElement.innerText = quantity;
+    }
+  }
+  // Function to add item to cart
+  function addToCart() {
+    var quantity = parseInt(document.getElementById('quantity').innerText);
+    alert("Added " + quantity + " item(s) to cart!");
+  }
